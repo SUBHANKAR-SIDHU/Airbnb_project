7fb3ecba56b81cb9480a1b8f42f240789261de5f
SUBHANKAR-SIDHU <subhankarsiddhu@gmail.com>
2026-07-25 23:08:17 +0530

    add review joi validation

--- schema.js
+++ schema.js
@@ -1,10 +1,21 @@
 const Joi = require("joi");
 module.exports.listingSchema = Joi.object({
-  title: Joi.string().min(3).max(100).required(),
-  description: Joi.string().min(10).max(1000).allow(""),
-  image: Joi.string().uri().allow(""),
-  price: Joi.number().positive().required(),
-  location: Joi.string().min(2).max(100).required(),
-  country: Joi.string().min(2).max(100).required(),
+  listing: Joi.object({
+    title: Joi.string().min(3).max(100).required(),
+    description: Joi.string().min(10).max(1000).allow(""),
+    image: Joi.string().uri().allow(""),
+    price: Joi.number().positive().required(),
+    location: Joi.string().min(2).max(100).required(),
+    country: Joi.string().min(2).max(100).required(),
+  }).required(),
 });
 
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    comment: Joi.string().trim().required(),
+
+    rating: Joi.number().min(1).max(5).required(),
+
+    createdAt: Joi.date().default(() => new Date()),
+  }).required(),
+});
