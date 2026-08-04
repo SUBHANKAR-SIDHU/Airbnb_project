f3963590e36f7339007592aee26b598f50ff9dd3
SUBHANKAR-SIDHU <subhankarsiddhu@gmail.com>
2026-08-04 09:10:08 +0530

    create user schema

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const ExpressError = require("./utils/ExpressError.js");
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
-const flash = require("connect-flash")
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/paradise";
 
@@ -45,15 +48,28 @@ const sessionOption = {
 };
 
 app.use(session(sessionOption));
-
 app.use(flash());
 
-app.use((req,res,next)=>{
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-})
+});
 
+app.get("/demo", async (req, res) => {
+  let fakeuser = new User({
+    email: "abc@gmail.com",
+    username: "abc",
+  });
+  let rigisterUser = await User.register(fakeuser, "helloworld");
+  res.send(rigisterUser);
+});
 
 app.use("/listings", listings);
 app.use("/listings/:id/review", reviews);
